85c695912ae3cf5a27b6eb54ffc86e5d8a28ce23
Kike Ferndandz <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-09-27 20:24:05 +0200

    Añadido Html basico de un boton y display para que se concatenen los datos en el mismo

---
+++ script.js
@@ -0,0 +1,13 @@
+const number = document.getElementById('number')
+const display = document.getElementById('display')
+
+
+number.addEventListener('click', () =>{
+
+    display.value += number.value
+
+
+
+})
+
+
