1c57de4baf0e1753ef8b6a407975d41338ba70a9
Kike Ferndandz <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2023-10-04 19:02:34 +0200

    Mejorada logita y html

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const display1 = document.getElementById("display1")
 const display2 = document.getElementById("display2")
-const equal = document.getElementById("equal")
 const numbers = document.querySelectorAll("#number")
+const sqr = document.getElementById("sqr")
+const raiz = document.getElementById("raiz")
+const percent = document.getElementById("percent")
+const clearAll = document.getElementById("clearAll")
+const clearError = document.getElementById("clearError")
 const del = document.getElementById("del")
-const clearAll = document.getElementById('clearAll')
-const clearError = document.getElementById('clearError')
-
-
+const invert = document.getElementById("invert")
+const equal = document.getElementById("equal")
+const changeSign = document.getElementById("changeSign")
 let resetdisplay2 = false
 
 numbers.forEach((number) => {
@@ -15,30 +18,62 @@ numbers.forEach((number) => {
       display2.value = ""
       resetdisplay2 = false
     }
-
     display2.value += number.value
   })
 })
 
 equal.addEventListener("click", () => {
-  try{display1.value = parseFloat(eval(display2.value))
-  resetdisplay2 = true
-  }catch{
-    alert('Error')
+  try {
+    display1.value = parseFloat(eval(display2.value))
+    resetdisplay2 = true
+  } catch {
+    alert("Error")
   }
 })
 
 del.addEventListener("click", () => {
   display2.value = display2.value.slice(0, -1)
-  console.log(display2.value)
+  
+  resetdisplay2 = false
 })
 
-clearAll.addEventListener('click', ()=>{
-  display1.value=''
-  display2.value=''
+clearAll.addEventListener("click", () => {
+  display1.value = ""
+  display2.value = ""
 })
 
-clearError.addEventListener('click', ()=>{
-  resetdisplay2=false
-  display1.value = '0'
+clearError.addEventListener("click", () => {
+  resetdisplay2 = false
+  display1.value = "0"
+})
+
+sqr.addEventListener("click", () => {
+  display1.value = Math.pow(eval(display2.value), 2)
+  display2.value = `sqr(${display2.value})`
+  resetdisplay2 = true
+})
+
+raiz.addEventListener("click", () => {
+  display1.value = Math.pow(eval(display2.value), 0.5)
+  display2.value = `raiz(${display2.value})`
+  resetdisplay2 = true
+})
+
+percent.addEventListener("click", () => {
+  display1.value = parseFloat(display2.value / 100)
+  resetdisplay2 = true
+})
+
+changeSign.addEventListener("click", () => {
+  if (display1.value !== 0){
+    display1.value = -1 * (display1.value)
+  }else{
+    display1.value
+
+  }
+})
+
+invert.addEventListener("click", () =>{
+ display2.value = parseFloat(1/(display2.value))
+ resetdisplay2 = true
 })
